12b034688b9e1a6c4e058005745f6ee3e2f546d3
Steven Starwalt <steven.starwalt@yahoo.com>
2018-01-23 22:36:45 -0800

    fixed the grand total, the total cookies sold row does not clear when adding a new row.

--- js/app.js
+++ js/app.js
@@ -26,7 +26,12 @@ LocationSalesData.prototype.render = function(){
   tdE1.textContent = this.locationName;
   trE1.appendChild(tdE1);
 
-  for (var i = 0; i < times.length; i++) {
+  for (var i = 0; i < locationSalesData.length; i++){
+    locationSalesData.totalCookiesSold = 0;
+  }
+
+  for (var j = 0; j < times.length; j++) {
+
 
     tdE1 = document.createElement('td');
 
@@ -40,11 +45,9 @@ LocationSalesData.prototype.render = function(){
 
     this.totalCookiesSold = this.totalCookiesSold + randomCookiesSold;
 
-    var timeTotalsNow = timeTotals[i] + randomCookiesSold;
+    var timeTotalsNow = timeTotals[j] + randomCookiesSold;
 
-    timeTotals[i] = timeTotalsNow;
-
-    grandTotal = grandTotal + timeTotalsNow;
+    timeTotals[j] = timeTotalsNow;
   }
 
   tdE1 = document.createElement('td'),
@@ -92,6 +95,7 @@ function makeTimeTotalsRow() {
     tdEl = document.createElement('td');
     tdEl.textContent = timeTotals[i];
     trEl.appendChild(tdEl);
+    grandTotal = grandTotal + timeTotals[i];
   }
 
   tdEl = document.createElement('td');
@@ -99,6 +103,7 @@ function makeTimeTotalsRow() {
   trEl.appendChild(tdEl);
 
   salesTable.appendChild(trEl);
+  grandTotal = 0;
 }
 
 function addNewLocation(event) {
@@ -111,6 +116,8 @@ function addNewLocation(event) {
   new LocationSalesData(newLocationName, newMinimumCustomers, newMaximumCustomers, newCookiesPerCustomer);
 
   salesTable.innerHTML = '';
+
+  timeTotals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
   makeHeaderRow();
   renderLocationSalesData();
